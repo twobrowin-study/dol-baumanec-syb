cd5fc54997a13ee4f60f20f5a3706271e52dde78
Egor Dubrovin <dubrovin.en@ya.ru>
2022-09-17 00:33:03 +0300

    Восстановление бота после ошибочного падения

--- python/main.py
+++ python/main.py
@@ -1,14 +1,26 @@
 import asyncio
+import traceback
 
 from bot import Bot
 from keyboard import Keyboard
 from phones import Phones
 
+async def bot_restart_sequence():
+    while True:
+        try:
+            await Bot.infinity_polling()
+        except asyncio.CancelledError:
+            # don't interfere with cancellations
+            raise
+        except Exception:
+            print("Caught exception")
+            traceback.print_exc()
+
 async def main():
     await asyncio.gather(
         Keyboard.update_valid(),
         Phones.update(),
-        Bot.infinity_polling()
+        bot_restart_sequence()
     )
 
 if __name__ == '__main__':
